1dbcf5da50b6218bb9752fa6a0c02cc22acf84c4
asyasocute <asya_bububu@proton.me>
2025-04-13 19:58:17 +0700

    idc why it doesn't work

--- src/settings.py
+++ src/settings.py
@@ -3,15 +3,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///:memory:")
-    SERVER_ADDRESS: str = os.getenv("SERVER_ADDRESS", "0.0.0.0")
-    SERVER_PORT: str = os.getenv("SERVER_PORT", "8000")
+    DATABASE_URL: str
+    # DATABASE_URL: str = os.getenv("DATABASE_URL")
+    # SERVER_ADDRESS: str = os.getenv("SERVER_ADDRESS", "0.0.0.0")
+    # SERVER_PORT: str = os.getenv("SERVER_PORT", "8000")
 
     def get_database_url(self):
         return self.DATABASE_URL
 
-    class Config:
-        env_file: str = ".env"
+    # class Config:
+    #     env_file: str = ".env"
 
 
 settings = Settings()
