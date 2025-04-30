3ef098b28e86545e95eb2d2a6cf9f31100178ff6
asyasocute <asya_bububu@proton.me>
2025-04-30 11:39:43 +0700

    schemas 2

--- src/api/notes.py
+++ src/api/notes.py
@@ -8,17 +8,33 @@ router = APIRouter(tags=["notes"])
 
 
 @router.get("/")
-def get_note(current_user: CurrentUser):  # pyright: ignore[reportUnusedParameter]
+def get_note(
+    note_id: int, current_user: CurrentUser  # pyright: ignore[reportUnusedParameter]
+):
     return "meow"
 
 
-@router.post("/")
+@router.get("/all")
+def get_notes(current_user: CurrentUser):  # pyright: ignore[reportUnusedParameter]
+    return "meow"
+
+
+@router.post("/create")
 def create_note(current_user: CurrentUser):  # pyright: ignore[reportUnusedParameter]
     return "meow"
 
 
-@router.patch("/")
-def edit_note(note_id: int, current_user: CurrentUser):
+@router.patch("/edit")
+def edit_note(
+    note_id: int, current_user: CurrentUser  # pyright: ignore[reportUnusedParameter]
+):
+    return "meow"
+
+
+@router.post("/delete")
+def delete_note(
+    note_id: int, current_user: CurrentUser  # pyright: ignore[reportUnusedParameter]
+):
     return "meow"
 
 
